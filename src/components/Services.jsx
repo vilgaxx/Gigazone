9fa9d8a940ba348512d1f7beb829af39a49c3ed3
vilgaxx <codervilgax@gmail.com>
2024-09-13 23:14:15 +0530

    correction

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -20,7 +20,7 @@ const Services = () => {
 
         <div className="relative">
           <div className="relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]">
-            <div className="absolute top-0 left-0 w-full h-full pointer-events-none md:w-3/5 xl:w-auto">
+            <div className="absolute top-0 left-0 w-full h-full pointer-events-none bg-gradient-to-b from-n-8/0 to-n-8/90 md:w-3/5 xl:w-auto">
               <img
                 className="w-full h-full object-cover md:object-right mix-blend-multiply"
                 width={800}
